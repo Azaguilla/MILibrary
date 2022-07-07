e6aeb7e9389c07a429edf8387c69024c1dbef58b
azaguilla <laurie.m1@free.fr>
2022-07-07 22:09:47 +0200

    Mise en page de la page home-not-logged-in et ajout du fond gris dans app-component"

--- app/src/app/authentication/authentication.module.ts
+++ app/src/app/authentication/authentication.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { AuthenticationRoutingModule } from './authentification-routing.module';
+import { NotLoggedInHomeComponent } from './components/not-logged-in-home/not-logged-in-home.component';
 
 
 @NgModule({
   declarations: [
     SignupComponent,
-    SigninComponent
+    SigninComponent,
+    NotLoggedInHomeComponent
   ],
   exports: [
     SignupComponent,
---
+++ app/src/app/authentication/components/not-logged-in-home/not-logged-in-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-logged-in-home',
+  templateUrl: './not-logged-in-home.component.html',
+  styleUrls: ['./not-logged-in-home.component.scss']
+})
+export class NotLoggedInHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
